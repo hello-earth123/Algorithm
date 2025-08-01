58ed0be06f7fb70cf26546ab8f9f47c70eca52d1
hello-earth123 <rock7501@naver.com>
2025-08-01 17:29:24 +0900

    algo0801

--- swea/D2/swea_1979_where-to-put-words.py
+++ swea/D2/swea_1979_where-to-put-words.py
@@ -1,61 +1,88 @@
 T = int(input())
 
 
-for test_case in range(T):
-    #N은 퍼즐 가로 세로 길이
-    #K는 단어 길이
-    N, K = list(map(int, input().split()))
-
-    puzzle = []
+for test_case in range(1, T + 1):
+    N, K = map(int, input().split())
+    puzzle = [list(map(int, input().split())) for _ in range(N)]
     result = 0
-    
-    for _ in range(N):   
-        #퍼즐 만들기
-        row = list(map(int, input().split()))
-        puzzle.append(row)
 
-    #비교 -  가로
+    # 가로 방향 검사
     for row in puzzle:
-        count = 0
-        for i in range(N):
-            if row[i] == 1:
-                count += 1
-            else:
-                if count == K:
+        # 임의로 양 끝에 0을 붙여서 양 옆에 0이 있다면 result에 1을 더하게 강제 -> 왜? 맨 처음과 끝은 어짜피 0인거나 마찬가지이다. 검사를 할 필요도 없이 참이기 때문
+        row = [0] + row + [0]  
+
+        # 그렇게 해서 원래 puzzle의 0번 index가 row = [0] + row + [0] 를 만나 1번 index로 바뀌었고 row의 길이는 N+2가 된다.
+        for i in range(1, N + 2 - K):
+            if row[i - 1] == 0 and row[i + K] == 0:
+                if all(x == 1 for x in row[i:i + K]):
                     result += 1
-                count = 0
-        if count == K:  # 행 끝까지 1이었을 때
-            result += 1
-    
-    
-    #비교 - 세로
-    
-    puzzle_col = list(zip(*puzzle)) # 세로 비교 하려고 transpose
-    
-    for col in puzzle_col:
-        count = 0
-        for i in range(N):
-            if col[i] == 1:
-                count += 1
-            else:
-                if count == K:
+
+    # 세로 방향 검사
+    for col in zip(*puzzle):
+        col = [0] + list(col) + [0]
+        for i in range(1, N + 2 - K):
+            if col[i - 1] == 0 and col[i + K] == 0:
+                if all(x == 1 for x in col[i:i + K]):
                     result += 1
-                count = 0
-        if count == K:
-            result += 1
+
+    print(f'#{test_case} {result}')
+
+
+# for test_case in range(T):
+#     #N은 퍼즐 가로 세로 길이
+#     #K는 단어 길이
+#     N, K = list(map(int, input().split()))
+
+#     puzzle = []
+#     result = 0
+    
+#     for _ in range(N):   
+#         #퍼즐 만들기
+#         row = list(map(int, input().split()))
+#         puzzle.append(row)
+
+#     #비교 -  가로
+#     for row in puzzle:
+#         count = 0
+#         for i in range(N):
+#             if row[i] == 1:
+#                 count += 1
+#             else:
+#                 if count == K:
+#                     result += 1
+#                 count = 0
+#         if count == K:  # 행 끝까지 1이었을 때
+#             result += 1
+    
+    
+#     #비교 - 세로
+    
+#     puzzle_col = list(zip(*puzzle)) # 세로 비교 하려고 transpose
+    
+#     for col in puzzle_col:
+#         count = 0
+#         for i in range(N):
+#             if col[i] == 1:
+#                 count += 1
+#             else:
+#                 if count == K:
+#                     result += 1
+#                 count = 0
+#         if count == K:
+#             result += 1
                     
      
-    print(f'#{test_case + 1} {result}')
+#     print(f'#{test_case + 1} {result}')
     
     
-# 원리는 이렇다
-# 만약 블럭 하나가 1이면 count에 1을 더한다.
-# 그렇지 않으면(블럭 하나가 0인 상황을 마주하면)
-# 만약 0을 마주했는데 count == K라면 result에 1을 더한다.
-# count를 초기화 한다.
-# for문을 다 돌았는데(하나의 row를 다 검사했는데) count == K이면 result에 1을 더한다.
+# # 원리는 이렇다
+# # 만약 블럭 하나가 1이면 count에 1을 더한다.
+# # 그렇지 않으면(블럭 하나가 0인 상황을 마주하면)
+# # 만약 0을 마주했는데 count == K라면 result에 1을 더한다.
+# # count를 초기화 한다.
+# # for문을 다 돌았는데(하나의 row를 다 검사했는데) count == K이면 result에 1을 더한다.
 
 
-# zip 함수는 개으르다. 꼭 list()로 덮어줘야 실행됨 -> 그리고 그 값을 변수에 저장을 해주자 꼭
-# 연속된 무언가를 구할 때는 연속이 깨지면 변수 초기화
-# 배열의 마지막에도 확인을 하려면 for문 밖에서 조건을 만족하는지 확인 하는 것도 기억하자.
+# # zip 함수는 개으르다. 꼭 list()로 덮어줘야 실행됨 -> 그리고 그 값을 변수에 저장을 해주자 꼭
+# # 연속된 무언가를 구할 때는 연속이 깨지면 변수 초기화
+# # 배열의 마지막에도 확인을 하려면 for문 밖에서 조건을 만족하는지 확인 하는 것도 기억하자.
--- wrong_answer_note.py
+++ wrong_answer_note.py
@@ -1,44 +1,57 @@
-from pprint import pprint
-
 T = int(input())
 
-for test_case in range(1, T+1):
-    #입력값, 배열 할당
-    N = int(input())
-    matrix = []
-    matrix_90 = []
-    matrix_180 = []
-    matrix_270 = []
+N, K = map(int, input().split())
 
-    # N*N 행렬 만들기
+for test_case in range(1, T+1):
+    puzzle = []
+    result = 0
+    is_result = True
+    # 퍼즐 생성 
+    # 0은 검은색, 1은 흰색, 검은색에는 못들어가고 흰색에만 들어감
+    # 글자 갯수가 딱 맞아야함
     for _ in range(N):
         lst = list(map(int, input().split()))
-        matrix.append(lst)
-
-    
-    # 90도 회전
-    for row in list(zip(*matrix)):
-        matrix_90.append(list(reversed(row)))
+        puzzle.append(lst)
 
 
-    # 180도 회전
-    for row in list(zip(*matrix_90)):
-        matrix_180.append(list(reversed(row)))
-    
-    
-    # 270도 회전
-    for row in list(zip(*matrix_180)):
-        matrix_270.append(list(reversed(row)))
+    # 가로 계산
+    for row in puzzle:
+        if all(x == 1 for x in row[:N-K+1]) and row[N-K+1] == 0:
+            result += 1
 
-    
-    print(f'#{test_case}')
-    for p in range(N):
-        for first in range(N):
-            print(f'{matrix_90[p][first]}', end = '')
-        print(end = ' ')
-        for second in range(N):
-            print(f'{matrix_180[p][second]}', end = '')  
-        print(end = ' ')
-        for third in range(N):     
-            print(f'{matrix_270[p][third]}', end = '')
-        print()
+        elif all(x == 1 for x in row[-K:]) and row[-K-1] == 0:
+            result += 1
+                
+        for i in range(1, N-K):
+            if all(x == 1 for x in row[i:i+K]) and row[i-1] == 0 and row[i+K] == 0:
+                is_result = True
+                if is_result == True:
+                    result += 1
+            
+            else:
+                is_result = False
+                if is_result ==False:
+                    continue
+
+
+    #세로 계산
+    puzzle_transpose = list(zip(*puzzle))
+    for col in puzzle_transpose:
+        if all(x == 1 for x in col[:N-K+1]) and col[N-K+1] == 0:
+            result += 1
+
+        elif all(x == 1 for x in col[-K:]) and col[-K-1] == 0:
+            result += 1
+                    
+        for i in range(1, N-K):
+            if all(x == 1 for x in col[i:i+K]) and col[i-1] == 0 and col[i+K] == 0:
+                is_result = True
+                if is_result == True:
+                    result += 1
+            
+            else:
+                is_result = False
+                if is_result ==False:
+                    continue
+
+    print(f'#{test_case} {result}')
