b59fcf3259d0f49c8fbaa4c2eca512df7d60b095
Junseo <rock7501@naver.com>
2025-08-11 00:48:53 +0900

    0810home

--- bjoon/silver/bjoon_1012_organic-cabage.py
+++ bjoon/silver/bjoon_1012_organic-cabage.py
@@ -1,33 +1,39 @@
-T = int(input())
+from collections import deque
 
-for test_case in range(1, T+1):
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    field[y][x] = 0
+
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+
+    while queue:
+        cx, cy = queue.popleft()
+        for i in range(4):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            if 0 <= nx < M and 0 <= ny < N and field[ny][nx] == 1:
+                field[ny][nx] = 0
+                queue.append((nx, ny))
+
+
+
+T = int(input())  # 테스트 케이스 수
+
+
+for _ in range(T):
     M, N, K = map(int, input().split())
+    field = [[0]*M for _ in range(N)]
     
-    
-    # 배추밭 만들기 / 패딩 상하좌우 한 칸씩
-    farm = []
-    farm.append([0] * (N + 2))
-    for _ in range(N):
-        row = [0] * M
-        new_row = [0] + row + [0]
-        farm.append(new_row)
-    farm.append([0] * (N + 2))
-        
-    
-    # 배추 심기
     for _ in range(K):
-        # 2차평면 좌표상으로 나타내주길래 -> 배열식 읽기로 바꿔주는 센스
-        c, r = map(int, input().split())
-        
-        farm[r+1][c+1] = 1
-        
-    # print(farm)
-    
-    
-    # 배추 벌레 갯수 count
-    is_worm = True
-    for r in range(1, M+1):
-        for c in range(1, N+1):
-            
-            pass
-    
+        x, y = map(int, input().split())
+        field[y][x] = 1  # 배추 심기
+
+    count = 0
+    for y in range(N):
+        for x in range(M):
+            if field[y][x] == 1:
+                bfs(x, y)
+                count += 1
+    print(count)
